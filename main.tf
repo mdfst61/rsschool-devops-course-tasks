3b022dfa9a233a85bc1f2e6e5689e46f6772c29c
Sergey Zaslonov <szaslonov@devexperts.com>
2025-06-15 00:10:45 +0300

    1-7

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         	   = "mdfst-rsschool-devops2025"
+    key                = "state/terraform.tfstate"
+    region         	   = "eu-north-1"
+    encrypt        	   = true
+    profile            = "mdfst_61"
+  }
+}
